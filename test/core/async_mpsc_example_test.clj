239a3935012bed7aacebce1be7e0533d38cd3106
Kevin Corcoran <kwcorcoran@gmail.com>
2015-12-14 10:58:34 -0800

    Initial project skeleton

---
+++ test/core/async_mpsc_example_test.clj
@@ -0,0 +1,7 @@
+(ns core.async-mpsc-example-test
+  (:require [clojure.test :refer :all]
+            [core.async-mpsc-example :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
